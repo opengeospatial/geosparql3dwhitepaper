cc7bc9dc7df557457fb50e39ac721bdfb09a6509
Timo Homburg <situx@users.noreply.github.com>
2025-06-03 17:38:01 +0200

    fix

--- scripts/processbibtex.py
+++ scripts/processbibtex.py
@@ -7,11 +7,21 @@ bibstring=""
 for entry in bibtexlib.entries:
     print(entry.fields)
     print(entry)
-    if "doi" in entry.fields:
-        bibstring+="* [[["+entry.key+", doi:"+entry.fields["doi"]+"]]]\n\n"
-    elif "DOI" in entry.fields:
-        bibstring+="* [[["+entry.key+", doi:"+entry.fields["DOI"]+"]]]\n\n"
+    founddoi=False
+    thedoi=None
+    for field in entry.fields:
+        if field.key=="doi" or field.key=="DOI":
+            thedoi=field.value
+            foundoi=True
+    if founddoi:
+        bibstring+="* [[["+entry.key+", doi:"+thedoi+"]]]\n\n"
     else:
         bibstring+="* [[["+entry.key+", local-file("+entry.key+")]]]\n\n"
+    #if "doi" in entry.fields:
+    #    bibstring+="* [[["+entry.key+", doi:"+entry.fields.doi+"]]]\n\n"
+    #elif "DOI" in entry.fields:
+    #    bibstring+="* [[["+entry.key+", doi:"+entry.fields["DOI"]+"]]]\n\n"
+    #else:
+    #    bibstring+="* [[["+entry.key+", local-file("+entry.key+")]]]\n\n"
 with open("document/sections/99-annex-bibliography.adoc","a") as bibdoc:
     bibdoc.write(bibstring)

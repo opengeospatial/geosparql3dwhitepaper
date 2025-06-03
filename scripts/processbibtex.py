bbc94d6f150195ab6cb9d01856b76a7ccb66be79
Timo Homburg <situx@users.noreply.github.com>
2025-06-03 17:09:25 +0200

    fixes

--- scripts/processbibtex.py
+++ scripts/processbibtex.py
@@ -7,6 +7,8 @@ bibstring=""
 for entry in bibtexlib.entries:
     if "doi" in entry.fields:
         bibstring+="* [[["+entry.key+", doi:"+entry.fields["doi"]+"]]]\n\n"
+    elif "DOI" in entry.fields:
+        bibstring+="* [[["+entry.key+", doi:"+entry.fields["DOI"]+"]]]\n\n"
     else:
         bibstring+="* [[["+entry.key+", local-file("+entry.key+")]]]\n\n"
 with open("document/sections/99-annex-bibliography.adoc","a") as bibdoc:

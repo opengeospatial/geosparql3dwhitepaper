3fd7d7a6755942760d7992f939bce2e3167bdead
Timo Homburg <situx@users.noreply.github.com>
2025-06-03 18:08:09 +0200

    applied bibtex bibliography

--- scripts/processbibtex.py
+++ scripts/processbibtex.py
@@ -5,26 +5,15 @@ with open("document/bibliography.bib", 'r') as file:
 bibtexlib=bibtexparser.parse_string(bibcontent)
 bibstring=""
 for entry in bibtexlib.entries:
-    print(entry.fields)
-    print(entry)
     founddoi=False
     thedoi=None
     for field in entry.fields:
-        print(field)
-        print(field.key)
         if field.key=="doi" or field.key=="DOI":
             thedoi=field.value
-            print("THEDOI: "+str(thedoi))
             founddoi=True
     if founddoi:
         bibstring+="* [[["+entry.key+", doi:"+thedoi+"]]]\n\n"
     else:
         bibstring+="* [[["+entry.key+", local-file("+entry.key+")]]]\n\n"
-    #if "doi" in entry.fields:
-    #    bibstring+="* [[["+entry.key+", doi:"+entry.fields.doi+"]]]\n\n"
-    #elif "DOI" in entry.fields:
-    #    bibstring+="* [[["+entry.key+", doi:"+entry.fields["DOI"]+"]]]\n\n"
-    #else:
-    #    bibstring+="* [[["+entry.key+", local-file("+entry.key+")]]]\n\n"
 with open("document/sections/99-annex-bibliography.adoc","a") as bibdoc:
     bibdoc.write(bibstring)

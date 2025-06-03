9c4c9d7bb142ef97e9a9c02b5bd3cdc8c3e8d965
Timo Homburg <situx@users.noreply.github.com>
2025-06-03 16:42:00 +0200

    fix

--- scripts/processbibtex.py
+++ scripts/processbibtex.py
@@ -1,10 +1,10 @@
 import bibtexparser
 
-with open("../document/bibliography.bib", 'r') as file:
+with open("document/bibliography.bib", 'r') as file:
     bibcontent = file.read()
 bibtexlib=bibtexparser.parse_string(bibcontent)
 bibstring=""
 for entry in bibtexlib.entries:
     bibstring+="* [[["+entry.key+", local-file("+entry.key+")]]]\n\n"
-with open("../document/sections/99-annex-bibliography.adoc","a") as bibdoc:
+with open("document/sections/99-annex-bibliography.adoc","a") as bibdoc:
     bibdoc.write(bibstring)
